73cfb572ef7aea96972e7c6b5659defa54cb2516
mleibert <mike.leibert@gmail.com>
2019-03-29 01:27:20 -0400

    0

--- mb.R
+++ mb.R
@@ -11,7 +11,7 @@ x <- t(spirals$x )
 
 
 
-X <- x; Y <- y; HL <- 1; nodes <- 5; batches <- 2; LR = 1.5
+X <- x; Y <- y; HL <- 1; nodes <- 5; batches <- 2; LR = .25
 
 	WB <- init.wgt( HL, nodes , X) 
 	W <- WB$W; B <- WB$B
@@ -35,19 +35,19 @@ X <- x; Y <- y; HL <- 1; nodes <- 5; batches <- 2; LR = 1.5
 	Costs <- rep(NA, 30000 * length(Xt))
 	
 	ST <-system.time( 
-	for( i in 1:400000) {
+	for( i in 1:100000) {
 		
 	BS[1:M] <- sample(M) 
  	for( k in 1:batches) {
 		Xt[[k]] <- X[, BS[   BS[,k] > 0 , k ] ]
 		Yt[[k]] <- Y[ BS[   BS[,k] > 0 , k ]  ] }
 
-	for( j in 1:length(Xt) ){
+	for( k in 1:length(Xt) ){
 
-		FP  <- fwd.prop( Xt[[j]] , HL , W, B, sigmoid, Sigmoid)
-		C2 <- Costs[ij] <- Cost( Yt[[j]] , FP$A[[ HL+1 ]], "Sigmoid")
+		FP  <- fwd.prop( Xt[[k]] , HL , W, B, sigmoid, Sigmoid)
+		C2 <- Costs[ij] <- Cost( Yt[[k]] , FP$A[[ HL+1 ]], "Sigmoid")
  		ij <- ij + 1
- 		BP <-  bk.prop(Xt[[j]], Yt[[j]], HL , W, B, FP$Z, FP$A , 
+ 		BP <-  bk.prop(Xt[[k]], Yt[[k]], HL , W, B, FP$Z, FP$A , 
 			sigmoid, Sigmoid ) 
 	for( j in 1:(HL + 1)  ){
 		B[[j]] <- B[[j]] - (LR  )* BP$dB[[j]]
@@ -56,13 +56,13 @@ X <- x; Y <- y; HL <- 1; nodes <- 5; batches <- 2; LR = 1.5
 
  
 beep("mario")
- v  <- v + 1
  
-FP$A[[2]]
-
-Sigmoid( X  , W[[2]] , B[[2]] )
-yhat <- ifelse( FP$A[[1]] > .5 , 1 , 0    )
+ 
+yhat <- fwd.prop( X , HL , W, B, sigmoid, Sigmoid)$A[[2]]
+yhat <- ifelse( yhat > .5 , 1 , 0    )
 sum(( as.vector(yhat) == y ) * 1) /  length(as.vector(Y))
 
+ fwd.prop( X , HL , W, B, sigmoid, Sigmoid)$A[[2]][
+	which( ( as.vector(yhat) == y ) == F)]
 
 
