a589520706f778e9705a90c5114d683368acdb03
mleibert <mike.leibert@gmail.com>
2019-04-17 20:21:27 -0400

    1

--- cloud/train.R
+++ cloud/train.R
@@ -3,54 +3,54 @@ library(tensorflow)
 sess = tf$Session()
 hello <- tf$constant('Hello, TensorFlow!')
 sess$run(hello)
-
+ 
 amazon <- read.csv("reviews.csv")
-
+ 
 one.hot <- function(Z){return(unname( as.matrix( 
   as.data.frame( t( model.matrix(~ as.factor(Z) + 0) ) ) ) )) }
-
+ 
 testsamples <- sample( 1:nrow(amazon)   , ((nrow(amazon) )/ 2 ))
 trainsamples <-  setdiff(  1:nrow(amazon)   , testsamples)
 ytrain <- amazon[trainsamples,]$Score
 ytest <- amazon[testsamples ,]$Score
-
+ 
 length(testsamples ) == length(trainsamples )
 samples <- amazon[,10]; rm(amazon)
-
+ 
 max_features <- 1000
 gc()
-
+ 
 tokenizer <- text_tokenizer( num_words = max_features   ) %>% 
 	fit_text_tokenizer(samples)
-
+ 
 oh_results <- texts_to_matrix(tokenizer,samples, mode = "tfidf")
 dim(oh_results)
-
-
-
+ 
+ 
+ 
 x_train <- oh_results[trainsamples,]
 x_test <- oh_results[testsamples ,]
 rm(oh_results)
-
+ 
 ytrain <- t( one.hot(ytrain ) )
 ytest <- t( one.hot(ytest) )
-
-
-
+ 
+ 
+ 
 batch_size <- 32
 embedding_dims <- 50
 filters <- 250
 kernel_size <- 3
 hidden_dims <- 250
-epochs <- 10
-
+epochs <- 15
  
-
+ 
+ 
 maxlen <- dim(x_train)[2]
-
-
-
-
+ 
+ 
+ 
+ 
 # 
 # model <- keras_model_sequential() %>%
 #   #layer_embedding(input_dim=max_features,
@@ -64,38 +64,37 @@ maxlen <- dim(x_train)[2]
 #   layer_dense(5 , activation = "softmax" )  
 # 
 
-
-## "Very simple. Just keep adding layers until the test error does not improve anymore."
-
+L1 = 0 
+L2 = 0.01
+DO = 0.01
+OP = "adam" 
 
 model <- keras_model_sequential() %>% 
-	layer_dense(400 , activation = "relu", input_shape = maxlen )  %>%
-		layer_dropout(0.2) %>%
- 	layer_dense(400 , activation = "relu" )  %>%
-		layer_dropout(0.2) %>%
-  layer_dense(400 , activation = "relu" )  %>%
-  layer_dropout(0.2) %>%
-  layer_dense(400 , activation = "relu" )  %>%
-  layer_dropout(0.2) %>%
-  layer_dense(400 , activation = "relu" )  %>%
-  layer_dropout(0.2) %>%
-  layer_dense(400 , activation = "relu" )  %>%
-  layer_dropout(0.2) %>%
-  layer_dense(400 , activation = "relu" )  %>%
-  layer_dropout(0.2) %>%
-  	layer_dense(5 , activation = "softmax" )  
-
-
-
+	layer_dense(400 , activation = "relu", input_shape = maxlen,
+	            kernel_regularizer = regularizer_l1_l2(l1 = L1, l2 = L2) )  %>%
+              layer_dropout(DO) %>% 
+  layer_dense(400 , activation = "relu", input_shape = maxlen,
+              kernel_regularizer = regularizer_l1_l2(l1 = L1, l2 = L2) )  %>%
+              layer_dropout(DO) %>%  
+  layer_dense(400 , activation = "relu", input_shape = maxlen ,
+              kernel_regularizer = regularizer_l1_l2(l1 = L1, l2 = L2))  %>%
+              layer_dropout(DO) %>% 
+  layer_dense(400 , activation = "relu", input_shape = maxlen,
+              kernel_regularizer = regularizer_l1_l2(l1 = L1, l2 = L2))  %>%
+              layer_dropout(DO) %>%
+  layer_dense(5 , activation = "softmax" )  
+ 
+ 
+ 
 # Compile model
 model %>% compile(
   loss = "categorical_crossentropy",
-  optimizer = "adagrad",
+  optimizer = OP,
   metrics = "accuracy"
 )
-
+ 
 # Training ----------------------------------------------------------------
-
+ 
 model %>%
   fit(
     x_train, (ytrain),
@@ -103,11 +102,9 @@ model %>%
     epochs = epochs ,
     validation_data = list(x_test, (ytest ) )
   )
-
-
+ 
+ 
 ## RNN's and embedding? What is pad sequences doing
-
-
+ 
+ 
 save_model_hdf5(model, 'model1.h5')
-
-
